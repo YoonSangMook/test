71a87e1d352347c795605f6c98698e3c980cae41
yoons <yoons@DESKTOP-D8NCBJC>
2023-02-28 10:57:27 +0900

    user22브랜치커밋22

--- test/src/main/java/com/example/demo/TestApplication.java
+++ test/src/main/java/com/example/demo/TestApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//user2브랜치에서 수정
+//user2브랜치에서 수정22
 
 @SpringBootApplication
 public class TestApplication {
