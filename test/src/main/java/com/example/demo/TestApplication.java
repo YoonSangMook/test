12836dd1ef6baa681822ab4f14e264a4b4e988c8
yoons <yoons@DESKTOP-D8NCBJC>
2023-03-09 12:57:29 +0900

    마스터브랜치에서 커밋

--- test/src/main/java/com/example/demo/TestApplication.java
+++ test/src/main/java/com/example/demo/TestApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //master브랜치와 origin/user1브랜치를 머지함
 //user2브랜치에서 수정22 수정33
 //user2에서 master브랜치와 origin/user2브랜치를 머지함
-
+//master브랜치에서 커밋
 
 @SpringBootApplication
 public class TestApplication {
